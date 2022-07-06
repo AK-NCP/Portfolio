64ba1b8a12f39ff2dde0d5c1ce4f1f001b7890c7
artem <pi7lon@gmail.com>
2022-07-06 21:16:52 +0300

    menu add functionality(scroll)

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 $(function (){
-    console.log("work")
+   $("[data-scroll]").on("click", function (event){
+       event.preventDefault();
+
+       let blockId = $(this).data("scroll");
+       let blockOffset = $(blockId).offset().top;
+        $("html, body").animate({
+            scrollTop: blockOffset - 50
+        }, 700);
+   });
 });
